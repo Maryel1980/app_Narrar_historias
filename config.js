1f55195588a41b7afb43d6b037d5acce0a4ba527
Maryel1980 <87047908+Maryel1980@users.noreply.github.com>
2022-07-19 16:18:14 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+   apiKey: "AIzaSyC_PtQeNPKFlrxuoiJWwufFM7WonlKIoJc",
+  authDomain: "apphistorias-e81bc.firebaseapp.com",
+  databaseURL: "https://apphistorias-e81bc-default-rtdb.firebaseio.com",
+  projectId: "apphistorias-e81bc",
+  storageBucket: "apphistorias-e81bc.appspot.com",
+  messagingSenderId: "171942602723",
+  appId: "1:171942602723:web:d75b15a419997e4895e2d6",
+};
